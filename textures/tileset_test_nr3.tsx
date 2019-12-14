860fd4646fd9153fa50f37928fcd1ed1ae1995a9
unknown <mihkelv01@gmail.com>
2019-12-14 18:00:42 +0200

    before collison

---
+++ textures/tileset_test_nr3.tsx
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tileset_test_nr3" tilewidth="32" tileheight="32" tilecount="70" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="stone2.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="stone3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="stone4.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="stone5.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="stone.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="stone1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="polished_andesite3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="polished_andesite.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="polished_andesite1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="polished_andesite2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="polished_diorite.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="polished_diorite1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="polished_diorite2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="polished_diorite3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="polished_granite.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="polished_granite1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="polished_granite2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="polished_granite3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="cobblestone.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="cobblestone1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="cobblestone2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="dirt.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="dirt1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="dirt2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="coarse_dirt.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="grass_block_side.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="bricks3.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="bricks.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="bricks1.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="bricks2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="cracked_stone_bricks.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="cracked_stone_bricks1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="cracked_stone_bricks2.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="cracked_stone_bricks3.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="cracked_stone_bricks4.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="cracked_stone_bricks5.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="32" source="cracked_stone_bricks_top.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="cracked_stone_bricks_top1.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="cracked_stone_bricks_top2.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="cracked_stone_bricks_top3.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="cracked_stone_bricks_top4.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="cracked_stone_bricks_top5.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="cracked_stone_bricks_top6.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="end_stone_bricks.png"/>
+ </tile>
+ <tile id="44">
+  <image width="32" height="32" source="end_stone_bricks1.png"/>
+ </tile>
+ <tile id="45">
+  <image width="32" height="32" source="end_stone_bricks2.png"/>
+ </tile>
+ <tile id="46">
+  <image width="32" height="32" source="end_stone_bricks3.png"/>
+ </tile>
+ <tile id="47">
+  <image width="32" height="32" source="end_stone_bricks4.png"/>
+ </tile>
+ <tile id="48">
+  <image width="32" height="32" source="end_stone_bricks5.png"/>
+ </tile>
+ <tile id="49">
+  <image width="32" height="32" source="end_stone_bricks6.png"/>
+ </tile>
+ <tile id="50">
+  <image width="32" height="32" source="end_stone_bricks7.png"/>
+ </tile>
+ <tile id="51">
+  <image width="32" height="32" source="oak_planks3.png"/>
+ </tile>
+ <tile id="52">
+  <image width="32" height="32" source="oak_planks4.png"/>
+ </tile>
+ <tile id="53">
+  <image width="32" height="32" source="oak_planks.png"/>
+ </tile>
+ <tile id="54">
+  <image width="32" height="32" source="oak_planks1.png"/>
+ </tile>
+ <tile id="55">
+  <image width="32" height="32" source="oak_planks2.png"/>
+ </tile>
+ <tile id="56">
+  <image width="32" height="32" source="jungle_planks.png"/>
+ </tile>
+ <tile id="57">
+  <image width="32" height="32" source="spruce_planks.png"/>
+ </tile>
+ <tile id="58">
+  <image width="32" height="32" source="acacia_planks.png"/>
+ </tile>
+ <tile id="59">
+  <image width="32" height="32" source="birch_planks.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="32" source="dark_oak_planks.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="32" source="bookshelf.png"/>
+ </tile>
+ <tile id="68">
+  <image width="32" height="32" source="bed.png"/>
+ </tile>
+ <tile id="69">
+  <image width="32" height="32" source="bed1.png"/>
+ </tile>
+ <tile id="70">
+  <image width="32" height="32" source="coal_ore.png"/>
+ </tile>
+ <tile id="71">
+  <image width="32" height="32" source="diamondore.png"/>
+ </tile>
+ <tile id="72">
+  <image width="32" height="32" source="goldore.png"/>
+ </tile>
+ <tile id="73">
+  <image width="32" height="32" source="ironore.png"/>
+ </tile>
+ <tile id="74">
+  <image width="32" height="32" source="lapisore.png"/>
+ </tile>
+ <tile id="75">
+  <image width="32" height="32" source="redstoneore.png"/>
+ </tile>
+</tileset>
